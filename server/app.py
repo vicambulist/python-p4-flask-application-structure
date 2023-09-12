1590eb172cae73fdf4338532054a97317cad559b
Bret Barden <bret.barden@gmail.com>
2023-09-12 14:05:36 -0400

    lab complete

--- server/app.py
+++ server/app.py
@@ -3,3 +3,19 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+# Overall route
+@app.route('/')
+def index():
+    return '<h1>Welcome to my page!</h1>'
+
+# This makes specific routes for users
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
+# Run a development server diretly through the script
+#   Also, the server will automaticall restart when
+#   a change is made to app.py
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
